2942502e910597fdaefb65c68064500277dcddf3
Stan <stan_roelofs@live.nl>
2018-08-16 11:18:21 +0200

    Modified GameBoy class such that it can load a new cartridge by calling loadCartridge, and implemented a function that automatically keeps running for a full frame.

--- src/main/kotlin/GameBoy.kt
+++ src/main/kotlin/GameBoy.kt
@@ -5,7 +5,7 @@ import java.io.File
 
 class GameBoy(cart: File) {
     val cpu = Cpu()
-    private val cart = Cartridge(cart)
+    private var cartridge = Cartridge(cart)
     val mmu = Mmu.instance
 
     init {
@@ -13,12 +13,25 @@ class GameBoy(cart: File) {
     }
 
     fun reset() {
-        mmu.cartridge = cart
+        mmu.cartridge = cartridge
         mmu.reset()
         cpu.reset()
     }
 
+    fun frame() {
+        val start = cpu.registers.clock
+        while (cpu.registers.clock - start < 70224) {
+            cpu.step()
+        }
+    }
+
     fun step() {
         cpu.step()
+    }
+
+    fun loadCartridge(cart: File) {
+        val temp = Cartridge(cart)
+        cartridge = temp
+        reset()
     }
 }
