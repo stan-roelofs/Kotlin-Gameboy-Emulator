3acdb224703ac8ff97e2a460c38e1fe1345b1625
Stan Roelofs <s.roelofs@student.tue.nl>
2019-09-12 18:05:21 +0200

    Changes to MemTiming2Tests which apparently outputs to different addresses in memory than the other Blargg tests

--- src/test/kotlin/blargg/MemTiming2Tests.kt
+++ src/test/kotlin/blargg/MemTiming2Tests.kt
@@ -1,24 +1,52 @@
 package blargg
 
+import GameBoy
+import org.junit.Assert
 import org.junit.Test
+import utils.Log
+import java.io.File
+import java.net.URI
 
 internal class MemTiming2Tests : BlarggTest() {
 
-    // TODO: these tests write output to a different location in memory than the other tests, so are reported to fail even though they actually pass
     override val path = "mem_timing-2/rom_singles"
 
     @Test
     fun test1() {
-        runBlarggTest("01-read_timing.gb")
+        runMemTest2("01-read_timing.gb")
     }
 
     @Test
     fun test2() {
-        runBlarggTest("02-write_timing.gb")
+        runMemTest2("02-write_timing.gb")
     }
 
     @Test
     fun test3() {
-        runBlarggTest("03-modify_timing.gb")
+        runMemTest2("03-modify_timing.gb")
+    }
+
+    private fun runMemTest2(fileName: String) {
+        val url: URI = BlarggTest::class.java.classLoader.getResource("blargg/${path}/${fileName}")!!.toURI()
+        val romFile = File(url)
+        val gb = GameBoy(romFile)
+
+        Log.i("")
+        Log.i("Running Blargg Test: $fileName")
+        Log.i("Using serial output to check pass/fail")
+
+
+        for (i in 0..100000000) {
+            gb.step()
+        }
+
+        val status = gb.mmu.readByte(0xA000)
+        println("Output status: $status")
+
+        if (gb.mmu.readByte(0xA001) != 0xDE || gb.mmu.readByte(0xA002) != 0xB0 || gb.mmu.readByte(0xA003) != 0x61) {
+            println("Output invalid, signature does not match")
+        }
+
+        Assert.assertEquals(0, status)
     }
 }
