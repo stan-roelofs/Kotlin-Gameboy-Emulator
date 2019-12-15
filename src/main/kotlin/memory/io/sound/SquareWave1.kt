4c6d8baa38b857fe71db3060fb3e122accd19b0d
Stan <stan_roelofs@live.nl>
2019-12-15 12:03:18 +0100

    Fixed failing test

--- src/main/kotlin/memory/io/sound/SquareWave1.kt
+++ src/main/kotlin/memory/io/sound/SquareWave1.kt
@@ -4,11 +4,14 @@ import memory.Mmu
 
 class SquareWave1 : SquareWave() {
 
+    private var NR0 = 0
+
     init {
         reset()
     }
 
     override fun reset() {
+        NR0 = 0
         super.reset()
         duty = 0b10
         volumeEnvelope.setNr2(0xF3)
@@ -18,8 +21,7 @@ class SquareWave1 : SquareWave() {
     override fun readByte(address: Int): Int {
         return when(address) {
             Mmu.NR10 -> {
-                0
-               // this.NR0 or 0b10000000
+                this.NR0 or 0b10000000
             }
             else -> super.readByte(address)
         }
@@ -29,7 +31,7 @@ class SquareWave1 : SquareWave() {
         val newVal = value and 0xFF
         when(address) {
             Mmu.NR10 -> {
-
+                NR0 = newVal
             }
             else -> super.writeByte(address, value)
         }
