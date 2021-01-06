4e718efaa68d2316da4c82ae2f715fe13643eb4c
Stan <stan@stanroelofs.nl>
2021-01-06 17:35:30 +0100

    Moved classes to one file

--- gameboy-lib/src/main/kotlin/gameboy/memory/io/graphics/PixelCGB.kt
+++
@@ -1,3 +0,0 @@
-package gameboy.memory.io.graphics
-
-class PixelCGB(color: Int, var palette: Int, priority: Boolean) : Pixel(color, priority)
--- gameboy-lib/src/main/kotlin/gameboy/memory/io/graphics/PixelDMG.kt
+++
@@ -1,3 +0,0 @@
-package gameboy.memory.io.graphics
-
-class PixelDMG (color : Int, var palette1: Boolean, priority: Boolean) : Pixel(color, priority)
